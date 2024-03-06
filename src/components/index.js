f13fa33b9f4512a5d7679fe59b82a8de3d48e372
unknown <riyashivolkar2@gmail.com>
2024-03-06 20:40:05 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as article } from "./article/Article";
+export { default as Brand } from "./brand/Brand";
+export { default as Cta } from "./cta/Cta";
+export { default as Feature } from "./feature/Feature";
+export { default as navbar } from "./navbar/Navbar";
